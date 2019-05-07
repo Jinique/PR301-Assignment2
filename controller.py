b157dc44ad2d84619da26ab11ee9b5fc086f6ab9
Jinique <44308010+Jinique@users.noreply.github.com>
2019-05-08 04:34:02 +1200

    v2.026 - update includes coverage html and outputs from application

--- controller.py
+++ controller.py
@@ -7,7 +7,6 @@ import re
 
 
 class Main(Cmd, Help):
-
     def __init__(self, username=None, root=None):
         Cmd.__init__(self)
         self.prompt = ">>> "
@@ -19,10 +18,12 @@ class Main(Cmd, Help):
 
     def do_interpret(self, line):
         if self.write_folder is None:
-            print("""Please enter the directory to write files to :
+            print(
+                """Please enter the directory to write files to :
 Syntax : write_folder [folder name]
 Example : write_folder out
-Result : Folder to write files are <root>/[folder name]""")
+Result : Folder to write files are /[folder name]"""
+            )
         elif self.source_file is None:
             print("Please enter the source file : source [file_name]")
         else:
@@ -64,7 +65,10 @@ Result : Folder to write files are <root>/[folder name]""")
             with open(self.source_file, "rt") as my_file:
                 if my_file.read().find("@startuml") != -1:
                     if self.root_directory:
-                        print(f"Source file to interpret is: {self.root_directory}/{line}")
+                        print(
+                            f"Source file to interpret is: "
+                            f"{self.root_directory}/{line}"
+                        )
                     else:
                         print(f"Source file to interpret is: {line}")
                 else:
@@ -94,9 +98,9 @@ Result : Folder to write files are <root>/[folder name]""")
 
     def file_path(self, line):
         path = []
-        for a_path in line.split(' '):
-            striped_path = re.sub(' ', '', a_path).strip()
-            if striped_path != '':
+        for a_path in line.split(" "):
+            striped_path = re.sub(" ", "", a_path).strip()
+            if striped_path != "":
                 path.append(striped_path)
                 # print(striped_path)
         return path
@@ -133,13 +137,13 @@ Result : Folder to write files are <root>/[folder name]""")
         return Cmd.cmdloop(self, intro)
 
 
-if __name__ == '__main__':  # pragma: no cover
-    if len(sys.argv) == 2:  # should not have functional code here. eg java main.
+if __name__ == "__main__":  # pragma: no cover
+    if len(sys.argv) == 2:  # shouldn't have functional code here eg.java main
         Main().cmdloop(sys.argv[1])  # so should call method from else where
     elif len(sys.argv) == 3:
         name = sys.argv[1] + " " + sys.argv[2]
         Main().cmdloop(name)
     elif len(sys.argv) > 3:
-            print("Error : cannot have more than 1 space in your name")
+        print("Error : cannot have more than 1 space in your name")
     else:
         Main().cmdloop("")
---
+++ output/interpreter.py
@@ -0,0 +1,43 @@
+class Interpreter:
+
+    def __init__(self):
+        self.my_file: str = ""  # ToDo
+        self.my_shelf = None  # ToDo
+        self.my_class_content: list = []  # ToDo
+        self.my_relationship_content: str = ""  # ToDo
+        self.all_my_errors: list = []  # ToDo
+        self.all_my_Modules = []
+        self.all_my_s = []
+
+    def add_file(self, file_name, new_module_name) ->None:
+        # ToDo
+        pass
+
+    def read_file(self) ->str:
+        # ToDo
+        pass
+
+    def find_relationship(self) ->str:
+        # ToDo
+        pass
+
+    def add_class(self, class_name, attributes, methods, relationships) ->None:
+        # ToDo
+        pass
+
+    def find_classes(self) ->tuple:
+        # ToDo
+        pass
+
+    def add_module(self, new_module_name, classes) ->None:
+        # ToDo
+        pass
+
+    def write_modules(self) ->None:
+        # ToDo
+        pass
+
+    def shelve_modules(self) ->None:
+        # ToDo
+        pass
+

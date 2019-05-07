83dbf6b41403050bd58038c5a1c367ab84440bf7
Jinique <44308010+Jinique@users.noreply.github.com>
2019-05-08 02:01:13 +1200

    v2.021 - creating doctest for controller

--- doctest_runner.py
+++ doctest_runner.py
@@ -1,13 +1,12 @@
-import doctest
-
-
 def test():
+    import doctest
     doctest.testfile('doctest_files/doctest_attribute', verbose=1)
     doctest.testfile('doctest_files/doctest_class_builder', verbose=1)
     doctest.testfile('doctest_files/doctest_interpreter', verbose=1)
     doctest.testfile('doctest_files/doctest_method', verbose=1)
     doctest.testfile('doctest_files/doctest_module', verbose=1)
     doctest.testfile('doctest_files/doctest_relationship', verbose=1)
+    doctest.testfile('doctest_files/doctest_controller', verbose=1)
 
 
 if __name__ == "__main__":
